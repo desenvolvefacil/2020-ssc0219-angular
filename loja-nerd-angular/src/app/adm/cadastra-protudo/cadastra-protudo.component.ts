ebb6c38edc8dd0e6767355ac6a30cc5573f808b9
wagner roberto balsani <wagnerb@sc.usp.br>
2020-12-04 01:02:55 -0300

    adm pages

--- loja-nerd-angular/src/app/adm/cadastra-protudo/cadastra-protudo.component.ts
+++ loja-nerd-angular/src/app/adm/cadastra-protudo/cadastra-protudo.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoriaService } from 'src/app/services/categoria.service';
+import { CategoriaModel } from 'src/model/CategoriaModel';
+import { ProdutoModel } from 'src/model/ProdutoModel';
 
 @Component({
   selector: 'app-cadastra-protudo',
@@ -7,9 +10,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastraProtudoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private catService: CategoriaService) {
+    this.categorias = new Array();
+    this.produto = new ProdutoModel();
+  }
+
+  categorias: Array<CategoriaModel>;
+
+  produto:ProdutoModel;
+
+
 
   ngOnInit(): void {
+    this.catService.lisarCategorias().subscribe((data: any) => {
+      this.categorias = data.map((e: any) => {
+        return {
+          Nome: e.payload.doc.data()['Nome'],
+          Alias: e.payload.doc.data()['Alias'],
+          //Description: e.payload.doc.data()['Description'],
+        };
+      })
+      //console.log(this.categorias);
+    });
   }
+
+  updateCategoria(inp:any){
+    if (inp != null && inp.returnValue) {
+      var input = inp.target as HTMLSelectElement;
+  
+      this.produto.AliasCategoria = input.value;
+
+      //alert(this.produto.AliasCategoria);
+  
+    }
+  }
+
+  atualizarFoto1(inp:any){
+    if (inp != null && inp.returnValue) {
+      var input = inp.target as HTMLInputElement;
+  
+      this.produto.Foto1 = input.value;
+
+      //alert(this.produto.AliasCategoria);
+  
+    }
+  }
+
+  
 
 }
