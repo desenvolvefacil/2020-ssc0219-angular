17aca636ed765e4e7cb5b65ca6d4c02e5e67c01d
Desenvolve Fácil <suporte@desenvolvefacil.com.br>
2020-12-05 17:08:30 -0300

    Listage e Verificação de Pedidos

--- loja-nerd-angular/src/app/adm/pedido-adm/pedido-adm.component.ts
+++ loja-nerd-angular/src/app/adm/pedido-adm/pedido-adm.component.ts
@@ -1,3 +1,7 @@
+import { PedidoModel } from './../../../model/PedidoModel';
+import { ActivatedRoute } from '@angular/router';
+import { PedidoService } from './../../services/pedido.service';
+import { UtilModel } from './../../../model/UtilModel';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PedidoAdmComponent implements OnInit {
 
-  constructor() { }
+  constructor(private pServ:PedidoService,public util:UtilModel,private route:ActivatedRoute) {
+
+    this.p = new PedidoModel();
+   }
+
+   p
 
   ngOnInit(): void {
+    let id = this.route.snapshot.paramMap.get('id');
+
+    if(id!=null){
+      this.p.IdPedido = id;
+
+      this.pServ.buscarPedido(id).get().subscribe((o:any)=>{
+        this.p.Data = o.data();
+      }
+      );
+
+    }
   }
+
+
 
 }
